7d173bff2aa6eb3042473d2b303944610d09bef6
fagundes321 <victorfagundes123@gmail.com>
2025-08-22 11:00:55 -0300

    Criando a Model

--- App/Models/Database/TypeDatabase/TypeDatabase.php
+++ App/Models/Database/TypeDatabase/TypeDatabase.php
@@ -1,1 +1,21 @@
-<?php 
+<?php
+
+namespace App\Models\Database\TypeDatabase;
+
+use App\Interfaces\InterfaceTypeDatabase;
+
+class TypeDatabase
+{
+
+    private $interfaceTypeDatabase;
+
+    public function __construct(InterfaceTypeDatabase $interfaceTypeDatabase)
+    {
+        $this->interfaceTypeDatabase = $interfaceTypeDatabase;
+    }
+
+    public function getDatabase()
+    {
+        return $this->interfaceTypeDatabase;
+    }
+}
---
+++ App/Models/Model.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Database\ConnectDatabase\ConnectPdoDatabase;
+use App\Models\Database\ConnectDatabase\ConnectMysqliDatabase;
+use App\Models\Database\TypeDatabase\TypeDatabase;
+use App\Models\Database\TypeDatabase\TypePdoDatabase;
+
+class Model
+{
+
+    private $typeDatabase;
+
+    public function __construct()
+    {
+
+        $database = new TypeDatabase(new TypePdoDatabase);
+        $this->typeDatabase = $database->getDatabase();
+    }
+
+    public function fetchAll()
+    {
+
+        $sql - "SELECT * FROM {$this->table}";
+        $this->typeDatabase->prepare($sql);
+        $this->typeDatabase->execute();
+        return $this->typeDatabase->fetchAll();
+    }
+
+    // select * from produtor where categoria = 1
+    public function find($field, $value, $fetch = null) 
+    {
+
+        $sql = "SELECT * FROM {$this->table} where $field = ?";
+        $this->typeDatabase->prepare($sql);
+        $this->typeDatabase->bindValue(1, $value);
+        $this->typeDatabase->execute();
+        return ($fetch == null) ? $this->typeDatabase->fetch() : $this->typeDatabase->fetchAll();
+    }
+}
