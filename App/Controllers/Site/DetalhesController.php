081a0ad6618c0259931000824da5d8e8488f9ae9
fagundes321 <victorfagundes123@gmail.com>
2025-09-18 16:55:38 -0300

    Problemas com o slug

--- App/Controllers/Site/DetalhesController.php
+++ App/Controllers/Site/DetalhesController.php
@@ -3,12 +3,22 @@
 namespace App\Controllers\Site;
 
 use App\Controllers\BaseController;
+use App\Models\Site\ProdutoModel;
 
 class DetalhesController extends BaseController{
 
+    private $produto;
+
+    public function __construct()
+    {
+        $this->produto = new ProdutoModel;
+    }
+
     public function index($slug){
 
-        dump($slug);
+        // $produtoEncontrado = $this->produto->find('produto_slug', $params[0]);
+       dump($slug);
+
     }
 
 }
