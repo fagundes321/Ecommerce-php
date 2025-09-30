7e7eec8c32fffd37f6b1929a1ef830f0001320b0
fagundes321 <victorfagundes123@gmail.com>
2025-09-30 16:28:34 -0300

    Feita a funcionalide carrinho

--- App/Classes/Carrinho.php
+++ App/Classes/Carrinho.php
@@ -4,51 +4,92 @@ namespace App\Classes;
 
 use App\Classes\StatusCarrinho;
 
+/**
+ * Classe responsável por gerenciar as operações do carrinho de compras
+ * 
+ * Funcionalidades:
+ * - Adicionar produtos
+ * - Atualizar quantidade
+ * - Remover produtos
+ * - Limpar carrinho
+ * - Consultar produtos no carrinho
+ */
 class Carrinho
 {
-
+    /**
+     * Objeto da classe StatusCarrinho
+     * Permite verificar se o carrinho existe e se um produto já está nele
+     */
     private $statusCarrinho;
 
     public function __construct()
     {
+        // Cria o objeto StatusCarrinho e garante que o carrinho exista na sessão
         $this->statusCarrinho = new StatusCarrinho();
         $this->statusCarrinho->criarCarrinho();
     }
 
+    /**
+     * Adiciona um produto ao carrinho
+     * Se o produto já existir, incrementa a quantidade
+     */
     public function add($id)
     {
         if ($this->statusCarrinho->produtoEstaNoCarrinho($id)) {
-            // se o produto já estiver no carrinho ele vai adicionar mais 1
+            // Produto já existe → adiciona 1 unidade
             $_SESSION['carrinho'][$id] += 1;
         } else {
-            // se  o produto não estiver no carrinho vai adicionar
+            // Produto não existe → adiciona 1 unidade
             $_SESSION['carrinho'][$id] = 1;
         }
     }
 
+    /**
+     * Retorna a quantidade de um produto específico no carrinho
+     */
     public function produtoCarrinho($id)
     {
         return $_SESSION['carrinho'][$id];
     }
 
     /**
-     * Atualiza a quantidade de produtos no carrinho
+     * Atualiza a quantidade de um produto no carrinho
+     * Só atualiza se o produto já estiver presente
      */
-    // public function update($id, $qtd){
-    //     if($this->statusCarrinho->produtoEstaNoCarrinho()){
-
-    //     }
-    // }
-
-    public function remove($id){
-
+    public function update($id, $qtd)
+    {
+        if ($this->statusCarrinho->produtoEstaNoCarrinho($id)) {
+            $_SESSION['carrinho'][$id] = $qtd;
+        }
     }
 
-    public function clear(){
-
+    /**
+     * Remove um produto do carrinho
+     */
+    public function remove($id)
+    {
+        if($this->statusCarrinho->produtoEstaNoCarrinho($id)){
+            unset($_SESSION['carrinho'][$id]);
+        }
     }
 
-    public function produtosCarrinho(){
-        
+    /**
+     * Limpa todo o carrinho
+     */
+    public function clear()
+    {
+        if($this->statusCarrinho->carrinhoExiste()){
+            unset($_SESSION['carrinho']);
+        }
+    }
+
+    /**
+     * Retorna todos os produtos do carrinho
+     */
+    public function produtosCarrinho()
+    {
+       if($this->statusCarrinho->carrinhoExiste()){
+        return $this->statusCarrinho->carrinho();
+       }
     }
 }
