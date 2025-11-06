5c310947400061f109c47467a234153db95bfe82
fagundes321 <victorfagundes123@gmail.com>
2025-11-06 13:36:35 -0300

    verificação de preenchimento do frete

--- App/Controllers/Site/FreteController.php
+++ App/Controllers/Site/FreteController.php
@@ -3,11 +3,24 @@
 namespace App\Controllers\Site;
 
 use App\Controllers\BaseController;
+use App\Repositories\Site\ProdutosCarrinhoRepository;
 
 class FreteController extends BaseController{
 
+    private $produtoCarrinhoRepository;
+
+    public function __construct()
+    {
+        $this->produtoCarrinhoRepository = new ProdutosCarrinhoRepository();
+    }
+    
     public function calcular(){
-        echo json_encode('Calcular frete');
+        $produtosCarrinho = $this->produtoCarrinhoRepository->produtosNoCarrinho();
+        
+        if(empty($produtosCarrinho)){
+            echo json_encode('produto');
+            die();
+        }
     }
 
 }
--- public/assets/js/site/frete.js
+++ public/assets/js/site/frete.js
@@ -1,40 +1,75 @@
 $(document).ready(function () {
 
-    var btn_calcular_frete = $('#btn-calcular-frete');
-    var mensagem_frete = $('#mensagem-frete');
+    var center_content = $(".center_content");
+    // var shopping_cart = center_content.find(".main-detalhes");
+    var btn_calcular_frete = center_content.find('#btn-calcular-frete');
+    var input_frete = center_content.find('#input-frete');
+    var mensagem_frete = center_content.find('#mensagem-frete');
+
+    // var btn_calcular_frete = $('#btn-calcular-frete');
+    // var mensagem_frete = $('#mensagem-frete');
 
     btn_calcular_frete.on('click', function (event) {
         event.preventDefault();
 
-        var cep = $('#input-frete').val().trim();
-
-        if (cep === '') {
-            mensagem_frete.text('Por favor, informe o CEP.').css('color', 'red');
-            return;
-        }
-
-        mensagem_frete.text('Calculando frete...').css('color', 'black');
-
+        var frete = input_frete.val();
         $.ajax({
             url: '/frete/calcular',
-            type: 'POST',
-            data: { frete: cep },  // <--- AGORA ENVIA O CEP CORRETAMENTE
+            type: 'post',
+            data: 'frete=' + frete,
             dataType: 'json',
             beforeSend: function () {
-                mensagem_frete.text('Calculando frete...');
+                if (frete === '') {
+                    mensagem_frete.html('Por favor, informe o CEP.').css('color', 'red');
+                    return;
+                }
+
+
+
             },
             success: function (retorno) {
-                console.log('Resposta do backend:', retorno);
-
-                if (retorno.sucesso) {
-                    mensagem_frete.text('Frete: R$ ' + retorno.valor).css('color', 'green');
-                } else {
-                    mensagem_frete.text('Não foi possível calcular o frete.').css('color', 'red');
+                console.log(retorno)
+                if (retorno == 'produto') {
+                    mensagem_frete.html('Você precisa ter produtos no carrinho.').css('color', 'red');
+                    return;
                 }
-            },
-            error: function () {
-                mensagem_frete.text('Erro ao conectar com o servidor.').css('color', 'red');
+
+                mensagem_frete.html('Calculando o frete...').css('color', 'black');
             }
         });
+
+
+
+
+        // var cep = $('#input-frete').val().trim();
+
+        // if (cep === '') {
+        //     mensagem_frete.text('Por favor, informe o CEP.').css('color', 'red');
+        //     return;
+        // }
+
+        // mensagem_frete.text('Calculando frete...').css('color', 'black');
+
+        // $.ajax({
+        //     url: '/frete/calcular',
+        //     type: 'POST',
+        //     data: { frete: cep },  // <--- AGORA ENVIA O CEP CORRETAMENTE
+        //     dataType: 'json',
+        //     beforeSend: function () {
+        //         mensagem_frete.text('Calculando frete...');
+        //     },
+        //     success: function (retorno) {
+        //         console.log('Resposta do backend:', retorno);
+
+        //         if (retorno.sucesso) {
+        //             mensagem_frete.text('Frete: R$ ' + retorno.valor).css('color', 'green');
+        //         } else {
+        //             mensagem_frete.text('Não foi possível calcular o frete.').css('color', 'red');
+        //         }
+        //     },
+        //     error: function () {
+        //         mensagem_frete.text('Erro ao conectar com o servidor.').css('color', 'red');
+        //     }
+        // });
     });
 });
