b042da8c716cc2636565dd08c5c566c900b4b2d1
fagundes321 <victorfagundes123@gmail.com>
2025-11-26 09:12:52 -0300

    Redirect do login

--- App/Controllers/Site/LoginController.php
+++ App/Controllers/Site/LoginController.php
@@ -6,6 +6,7 @@ use App\Controllers\BaseController;
 use App\Classes\Login;
 use App\Classes\Filters;
 use App\Models\Site\UserLogin;
+use App\Classes\Redirect;
 
 class LoginController extends BaseController
 {
@@ -26,8 +27,9 @@ class LoginController extends BaseController
     public function logar()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+            
             $filter = new Filters();
+            $redirect = new Redirect;
 
             $email = $filter->filter('email', 'string');
             $password = $filter->filter('password', 'string');
@@ -36,11 +38,12 @@ class LoginController extends BaseController
             $login->setEmail($_POST['email']);
             $login->setPassword($_POST['password']);
 
-            if($login->logar(new userLogin())){
-                header('location:/');
+            if ($login->logar(new userLogin())) {
+                return $redirect->redirect('/');
+            } else {
+                return $redirect->redirect('/login');
             }
-           header('location:/asf');
+            return $redirect->redirect('/');
         }
-        // header('location:/');
     }
 }
