947c54bfb832379ccdcfd567109c792792d2b22b
fagundes321 <victorfagundes123@gmail.com>
2025-11-27 12:38:59 -0300

    Configurando para listar os produtos por categoria

--- App/Controllers/Site/CategoriaController.php
+++ App/Controllers/Site/CategoriaController.php
@@ -1,19 +1,35 @@
-<?php 
+<?php
 
 namespace App\Controllers\Site;
 
 use App\Controllers\BaseController;
 use App\Models\Site\ProdutoModel;
 use App\Models\Site\CategoriaModel;
+use App\Classes\Redirect;
 
-class CategoriaController extends BaseController{
+class CategoriaController extends BaseController
+{
 
     public function index($params)
     {
+
         $categoriaModel = new CategoriaModel;
-        $categoriaEncontrada = $categoriaModel->find('categoria_slug', $params[0]);
-        dump($categoriaEncontrada);
+        $categoriaEncontrada = $categoriaModel->find('categoria_slug', $params);
 
+        $redirect = new Redirect;
+
+        if (!$categoriaEncontrada) {
+            $redirect->redirect("/");
+        }
+
+        $produtoModel = new ProdutoModel();
+        $produtosEncontrados = $produtoModel->find('produto_categoria', $categoriaEncontrada->id);
+
+        $dados = [
+            'produtos' => $produtosEncontrados,
+            'categoria' => $categoriaEncontrada
+        ];
+
+         echo $this->twig->render('site_categoria.html', $dados);
     }
-
 }
